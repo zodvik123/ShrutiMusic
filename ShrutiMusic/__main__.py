1ad4b07faa513d1a8fc3da7820ce844764a16a79
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 <duhgfhfdghg+A1@gmail.com>
2025-05-28 19:11:52 +0530

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/__main__.py
+++ ShrutiMusic/__main__.py
@@ -1,8 +1,6 @@
 import asyncio
 import importlib
-import threading
 
-from flask import Flask
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
@@ -14,17 +12,7 @@ from ShrutiMusic.plugins import ALL_MODULES
 from ShrutiMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
-# ------------ Flask Dummy Server for Render/Koyeb ------------ #
-flask_app = Flask(__name__)
 
-@flask_app.route("/")
-def home():
-    return "\xe2\x9c\x85\x20\x53\x68\x72\x75\x74\x69\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x69\x73\x20\x41\x6c\x69\x76\x65\x21"
-
-def run_flask():
-    flask_app.run(host="0.0.0.0", port=8000)
-
-# ------------ Bot Init ------------ #
 async def init():
     if (
         not config.STRING1
@@ -35,7 +23,6 @@ async def init():
     ):
         LOGGER(__name__).error("Assistant client variables not defined, exiting...")
         exit()
-
     await sudo()
     try:
         users = await get_gbanned()
@@ -46,37 +33,30 @@ async def init():
             BANNED_USERS.add(user_id)
     except:
         pass
-
     await app.start()
-
     for all_module in ALL_MODULES:
-        importlib.import_module("ShrutiMusic.plugins." + all_module)
+        importlib.import_module("ShrutiMusic.plugins" + all_module)
     LOGGER("ShrutiMusic.plugins").info("Successfully Imported Modules...")
-
     await userbot.start()
     await Aviax.start()
     try:
         await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("ShrutiMusic").error(
-            "Please turn on the videochat of your log group/channel.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
         exit()
     except:
         pass
-
     await Aviax.decorators()
     LOGGER("ShrutiMusic").info(
-        "\x53\x68\x72\x75\x74\x69\x20\x4d\x75\x73\x69\x63\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x0a\x44\x6f\x6e\x27\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x53\x68\x72\x75\x74\x69\x42\x6f\x74\x73"
+        "\x53\x68\x72\x75\x74\x69\x20\x4d\x75\x73\x69\x63\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x0a\x0a\x44\x6f\x6e\x27\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x53\x68\x72\x75\x74\x69\x42\x6f\x74\x73"
     )
-
     await idle()
-
     await app.stop()
     await userbot.stop()
     LOGGER("ShrutiMusic").info("Stopping Shruti Music Bot...")
 
 
 if __name__ == "__main__":
-    threading.Thread(target=run_flask).start()
     asyncio.get_event_loop().run_until_complete(init())
