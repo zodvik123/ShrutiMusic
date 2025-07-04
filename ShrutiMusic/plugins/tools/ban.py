90aa9d21aff2b14284cf2f34a1dbf29f66c0d546
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 : NoxxOP <duhgfhfdghg+A1@gmail.com>
2025-07-04 10:21:30 +0530

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/plugins/tools/ban.py
+++ ShrutiMusic/plugins/tools/ban.py
@@ -25,7 +25,7 @@ from ShrutiMusic.utils.functions import (
     extract_user_and_reason,
     time_converter,
 )
-from utils.permissions import adminsOnly, member_permissions
+from ShrutiMusic.utils.permissions import adminsOnly, member_permissions
 from config import BANNED_USERS
 
 warnsdb = mongodb.warns
