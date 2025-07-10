0feb3676cface92eb6fee31453a41ed94a7a09b5
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 : NoxxOP <duhgfhfdghg+A1@gmail.com>
2025-07-10 22:52:13 +0530

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/plugins/bot/botlogs.py
+++ ShrutiMusic/plugins/bot/botlogs.py
@@ -27,7 +27,7 @@ from config import LOG_GROUP_ID
 from ShrutiMusic import app
 from ShrutiMusic.utils.database import add_served_chat, get_assistant
 
-welcome_photo = "https://files.catbox.moe/ajobub.jpg"  # Bot added image
+welcome_photo = "https://files.catbox.moe/ajobub.jpg"
 
 @app.on_message(filters.new_chat_members, group=-10)
 async def join_watcher(_, message):
@@ -76,3 +76,38 @@ async def join_watcher(_, message):
 
     except Exception as e:
         print(f"Error: {e}")
+
+
+from pyrogram.types import Message
+from ChampuMusic.utils.database import delete_served_chat, get_assistant
+
+photo = [
+    "https://telegra.ph/file/1949480f01355b4e87d26.jpg",
+    "https://telegra.ph/file/3ef2cc0ad2bc548bafb30.jpg",
+    "https://telegra.ph/file/a7d663cd2de689b811729.jpg",
+    "https://telegra.ph/file/6f19dc23847f5b005e922.jpg",
+    "https://telegra.ph/file/2973150dd62fd27a3a6ba.jpg",
+]
+
+
+@app.on_message(filters.left_chat_member, group=-12)
+async def on_left_chat_member(_, message: Message):
+    try:
+        userbot = await get_assistant(message.chat.id)
+
+        left_chat_member = message.left_chat_member
+        if left_chat_member and left_chat_member.id == (await app.get_me()).id:
+            remove_by = (
+                message.from_user.mention if message.from_user else "𝐔ɴᴋɴᴏᴡɴ 𝐔sᴇʀ"
+            )
+            title = message.chat.title
+            username = (
+                f"@{message.chat.username}" if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐂ʜᴀᴛ"
+            )
+            chat_id = message.chat.id
+            left = f"✫ <b><u>#𝐋ᴇғᴛ_𝐆ʀᴏᴜᴘ</u></b> ✫\n\n𝐂ʜᴀᴛ 𝐓ɪᴛʟᴇ : {title}\n\n𝐂ʜᴀᴛ 𝐈ᴅ : {chat_id}\n\n𝐑ᴇᴍᴏᴠᴇᴅ 𝐁ʏ : {remove_by}\n\n𝐁ᴏᴛ : @{app.username}"
+            await app.send_photo(LOGGER_ID, photo=random.choice(photo), caption=left)
+            await delete_served_chat(chat_id)
+            await userbot.leave_chat(chat_id)
+    except Exception as e:
+        return
