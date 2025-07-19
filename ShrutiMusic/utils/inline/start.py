6a918aa61dc0a022f6f73682988ba4df541ad981
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 : NoxxOP <duhgfhfdghg+A1@gmail.com>
2025-07-19 09:01:06 +0530

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -83,3 +83,19 @@ def about_panel(_):
         ]
     ]
     return buttons
+
+def owner_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(text=_["S_H_1"], url=config.INSTAGRAM),
+            InlineKeyboardButton(text=_["S_H_2"], url=config.YOUTUBE),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_H_3"], url=config.GITHUB),
+            InlineKeyboardButton(text=_["S_H_4"], url=config.DONATE),
+        ],
+        [
+            InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="settingsback_helper")
+        ]
+    ]
+    return buttons
